2347ebb470c86fd91d1eb639006911ea7329cca7
Rodrigo Gerke <rodrigogerke18@gmail.com>
2025-11-08 17:55:33 -0300

    meus modulos (incompleto)

---
+++ modulos/atividade2.py
@@ -0,0 +1,10 @@
+import random
+
+def gerador(quantidade):
+    numeros = []
+    
+    for _ in range(quantidade):
+        numero = random.getrandbits(64) s
+        numeros.append(numero)
+        
+    return gerador random
---
+++ modulos/meus-modulos.py
@@ -0,0 +1,8 @@
+#atividade_1
+
+import random
+
+def atividade_1():
+    n = random.randint(5,10)
+    return n
+
